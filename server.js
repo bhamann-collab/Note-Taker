06e70278611bf1167ecce028506b13ca5cdaf1e7
bhamann-collab <brockohamann@gmail.com>
2020-05-06 13:56:32 +0930

    can now save Note data to the json file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bp = require("body-parser")
+const fs = require('fs');
 
 const port = 5000;
 
@@ -16,6 +17,25 @@ app.get("/", (req, res) => {
 
 app.get("/notes", (req, res) => {
     res.sendFile(__dirname + '/public/notes.html')
+})
+
+app.post("/api/notes", (req, res) => {
+    console.log("we posting bois")
+    var person = JSON.stringify({
+        "firstName": "John",
+        "lastName": "Doe",
+        "age": 50,
+        "eyeColor": "blue"
+      }, null, 2);
+
+      console.log(req.body)
+    
+    fs.writeFileSync('db.json', JSON.stringify(req.body, null, 2), 'utf8');
+
+})
+
+app.get("/api/notes", (req, res) => {
+    console.log("we getting bois")
 })
 
 app.listen(port, () => {
