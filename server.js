3c69c510618f16fac565a62c0a1a4940219c874d
bhamann-collab <brockohamann@gmail.com>
2020-05-08 00:46:25 +0930

    Made the server.js file nice and clean

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bp = require("body-parser")
 const fs = require('fs');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express()
 
